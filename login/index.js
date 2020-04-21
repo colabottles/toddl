a0e789142ec1a34cebb56295aca2568386de3ee7
Todd Libby <toddlibby@protonmail.com>
2020-04-21 14:17:52 -0400

    Trying to fix redirect error.

--- login/index.js
+++ login/index.js
@@ -22,7 +22,7 @@ toggle.addEventListener("change", toggleAll, false);
 
 function loginForm(){
   var login = document.querySelector("login").value;
-  window.location.protocol + "//" + window.location.host + "/" + window.location.pathname = "/login/success.html";
+  window.location.protocol + "//" + window.location.host + "/login/success.html";
 }
 
 // form.addEventListener("bouncerFormValid", function(form) {
