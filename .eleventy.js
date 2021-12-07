d7bd2a96a2ca1f25b541b7fdc057c110adb4b3b8
Todd Libby <toddlibby@protonmail.com>
2021-12-07 05:14:45 -0700

    Updated dependencies and edits to eleventy.js file.

--- .eleventy.js
+++ .eleventy.js
@@ -1,39 +1,44 @@
 const { DateTime } = require("luxon");
-const fs = require("fs");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  eleventyConfig.setDataDeepMerge(true);
+// Import filters
 
-  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+module.exports = function (config) {
+  config.addPlugin(pluginRss);
+  config.addPlugin(syntaxHighlight);
+  config.setDataDeepMerge(true);
 
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLLL, yyyy");
+  config.addLayoutAlias("post", "layouts/post.njk");
+
+  config.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLLL, yyyy"
+    );
   });
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('dd LLLL, yyyy');
+  config.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLLL, yyyy"
+    );
   });
 
   // Get the first `n` elements of a collection.
-  eleventyConfig.addFilter("head", (array, n) => {
-    if( n < 0 ) {
+  config.addFilter("head", (array, n) => {
+    if (n < 0) {
       return array.slice(n);
     }
 
     return array.slice(0, n);
   });
 
-  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
+  config.addCollection("tagList", require("./_11ty/getTagList"));
 
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("js");
-  eleventyConfig.addPassthroughCopy("fonts");
+  config.addPassthroughCopy("img");
+  config.addPassthroughCopy("css");
+  config.addPassthroughCopy("js");
+  config.addPassthroughCopy("fonts");
 
   /* Markdown Plugins */
   let markdownIt = require("markdown-it");
@@ -41,31 +46,32 @@ module.exports = function(eleventyConfig) {
   let options = {
     html: true,
     breaks: true,
-    linkify: true
+    linkify: true,
   };
   let opts = {
     permalink: false,
     permalinkClass: "direct-link",
-    permalinkSymbol: "#"
+    permalinkSymbol: "#",
   };
 
-  eleventyConfig.setLibrary("md", markdownIt(options)
-    .use(markdownItAnchor, opts)
+  config.setLibrary(
+    "md",
+    markdownIt(options).use(markdownItAnchor, opts)
   );
 
-  eleventyConfig.setBrowserSyncConfig({
-    callbacks: {
-      ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('_site/404.html');
+  // config.setBrowserSyncConfig({
+  //   callbacks: {
+  //     ready: function (err, browserSync) {
+  //       const content_404 = fs.readFileSync("_site/404.html");
 
-        browserSync.addMiddleware("*", (req, res) => {
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
-      }
-    }
-  });
+  //       browserSync.addMiddleware("*", (req, res) => {
+  //         // Provides the 404 content without redirect.
+  //         res.write(content_404);
+  //         res.end();
+  //       });
+  //     },
+  //   },
+  // });
 
   return {
     templateFormats: ["md", "njk", "html", "liquid", "js"],
@@ -84,7 +90,7 @@ module.exports = function(eleventyConfig) {
       input: ".",
       includes: "_includes",
       data: "_data",
-      output: "_site"
-    }
+      output: "_site",
+    },
   };
 };
