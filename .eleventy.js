72e530d8a42708f1d2f0cf28f442677145256252
Todd Libby <toddlibby@protonmail.com>
2025-10-13 07:43:16 -0700

    Update OG tweak

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,32 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
 const { DateTime } = require("luxon");
 const fs = require("fs");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const Image = require("@11ty/eleventy-img");
+
+async function shareImageShortcode(src) {
+  // src might be small.png - taken from frontmatter
+  const { url } = this.page;
+  // url might be /blog/hello-world/
+  const imageSrc = "." + url + src;
+  let metadata = await Image(imageSrc, {
+    widths: [600],
+    formats: ["jpeg, png, gif, webp"],
+    urlPath: url,
+    outputDir: `./_site/${url}`,
+  });
+
+  const data = metadata.jpeg[0];
+  // data.url might be /blog/hello-world/xfO_genLg4-600.jpeg
+  // note the filename is a content hash-width combination
+  return data.url;
+}
+
+export default function (eleventyConfig) {
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+};
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
@@ -75,10 +100,15 @@ const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
 
 module.exports = function(eleventyConfig) {
   // If you have other `addPlugin` calls, UpgradeHelper should be listed last.
+  eleventyConfig.addNunjucksAsyncShortcode(
+    "shareImageUri",
+    shareImageShortcode
+  );
   eleventyConfig.addPlugin(UpgradeHelper);
 };
 
   return {
+    markdownTemplateEngine: "njk",
     templateFormats: ["md", "njk", "html", "liquid", "js"],
 
     // If your site lives in a different subdirectory, change this.
