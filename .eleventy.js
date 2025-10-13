07f9146ba7bd8c4adc85be3f4b93599ed10ea581
Todd Libby <toddlibby@protonmail.com>
2025-10-13 09:23:09 -0700

    Please

--- .eleventy.js
+++ .eleventy.js
@@ -4,24 +4,6 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
 
-async function shareImageShortcode(src) {
-  // src might be small.png - taken from frontmatter
-  const { url } = this.page;
-  // url might be /blog/hello-world/
-  const imageSrc = "." + url + src;
-  let metadata = await Image(imageSrc, {
-    widths: [600],
-    formats: ["jpeg, png, gif, webp"],
-    urlPath: url,
-    outputDir: `./_site/${url}`,
-  });
-
-  const data = metadata.jpeg[0];
-  // data.url might be /blog/hello-world/xfO_genLg4-600.jpeg
-  // note the filename is a content hash-width combination
-  return data.url;
-}
-
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
