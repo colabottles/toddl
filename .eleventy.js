70ab7036b7a27babb543ffac2bd2051883730b08
Todd Libby <toddlibby@protonmail.com>
2025-10-13 10:13:02 -0700

    Trying new og image plugin again...again

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,7 @@ module.exports = function(eleventyConfig) {
 			fonts: [
 			{
 				name: 'Cal Sans',
-			  data: fs.readFileSync('https://fonts.googleapis.com/css2?family=Cal+Sans&family=Inter+Tight:ital,wght@0,100..900;1,100..900&display=swap'),
+			  data: fs.readFileSync('https://fonts.googleapis.com/css2?family=Cal+Sans&display=swap'),
 			  weight: 400,
 			  style: 'normal',
 			},
